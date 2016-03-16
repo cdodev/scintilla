9b4876a74a561184cf775bcafddab8f54a442f3d
Ben Ford <ben@perurbis.com>
2016-03-16 05:56:40 +0000

    Get working with haskell/default.nix

--- default.nix
+++ default.nix
@@ -1,2 +1,24 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7102" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./scintilla.nix { }
+{ mkDerivation, aeson, attoparsec, base, bytestring, containers
+, either, errors, free, hedis, lens, mmorph, mtl, pipes
+, pipes-concurrency, postgresql-simple, stdenv, tasty, tasty-hspec
+, tasty-quickcheck, text, transformers, unordered-containers, uuid
+}:
+mkDerivation {
+  pname = "scintilla";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  buildDepends = [
+    aeson attoparsec base bytestring containers either errors free
+    hedis lens mmorph mtl pipes pipes-concurrency postgresql-simple
+    text transformers unordered-containers uuid
+  ];
+  testDepends = [
+    base containers mmorph tasty tasty-hspec tasty-quickcheck
+    unordered-containers
+  ];
+  homepage = "http://github.com/cdodev/scintilla";
+  description = "Initial project template from stack";
+  license = stdenv.lib.licenses.bsd3;
+}
--- src/Scintilla/Command/Types.hs
+++ src/Scintilla/Command/Types.hs
@@ -1,17 +1,18 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
 module Scintilla.Command.Types
     ( CommandDb(..)
     , CommandT(..)
+    , runQ
     ) where
 
-import qualified Database.PostgreSQL.Simple as PG
-import Control.Monad.Trans.Reader
-import Control.Monad.Trans.State.Strict
-import           Control.Monad.Reader      (MonadReader(..))
-import Control.Monad.State.Strict (MonadState(..))
-import           Control.Monad.IO.Class     (MonadIO, liftIO)
-import           Control.Monad.Trans.Either (EitherT (..), eitherT,
-                                              left)
+import           Control.Monad.IO.Class           (MonadIO, liftIO)
+import           Control.Monad.Reader             (MonadReader (..))
+import           Control.Monad.State.Strict       (MonadState (..))
+import           Control.Monad.Trans.Either       (EitherT (..), eitherT, left)
+import           Control.Monad.Trans.Reader
+import           Control.Monad.Trans.State.Strict
+import qualified Database.PostgreSQL.Simple       as PG
 
 
 class CommandDb a where
@@ -25,3 +26,4 @@ newtype CommandT env err evt m a = CommandT {
   } deriving (Functor, Applicative, Monad, MonadIO, MonadReader env, MonadState CommandState)
 
 
+runQ a q = PG.query_ (getDb q) "select * from a"

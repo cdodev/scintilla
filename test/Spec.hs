a7b772e1ba987a54ecfe2de44d0c5fd94ab45542
Ben Ford <ben@perurbis.com>
2016-10-16 19:45:13 +0100

    Use tasty and tasty-ant-xml for tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
+module Main (main) where
+
+import Test.Tasty
+import Test.Tasty.Hspec
+import Test.Tasty.Runners.AntXML
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented in gocd"
+main = do
+  theSpecs <- testSpec "Specs" specs
+  defaultMainWithIngredients [antXMLRunner] theSpecs
+
+specs :: Spec
+specs = do
+  describe "A test" $ do
+    it "passes" $ (1::Int) `shouldBe` 2
